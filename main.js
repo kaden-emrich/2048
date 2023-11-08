628565ff4ca665e918913a596b444c4ed3cb5711
Kaden Emrich <kadenemrich@gmail.com>
2023-11-08 14:10:47 -0500

    movement and spawning

---
+++ main.js
@@ -0,0 +1,142 @@
+const gameDiv = document.getElementById('game');
+
+var squares = [[]];
+var values = [];
+
+function initGrid() {
+    gameDiv.innerHTML = '';
+    squares = [[]];
+    values = [];
+
+    for(let i = 0; i < 4; i++) {
+        squares[i] = [];
+        values[i] = [];
+
+        for(let j = 0; j < 4; j++) {
+            values[i][j] = 0;
+
+            squares[i][j] = document.createElement('div');
+            squares[i][j].classList.add('gridSquare');
+
+            gameDiv.appendChild(squares[i][j]);
+        }
+    }
+}
+
+function updateSquares() {
+    for(let i = 0; i < values.length; i++) {
+        for(let j = 0; j < values[i].length; j++) {
+            if(values[i][j] != 0) {
+                squares[i][j].innerText = values[i][j];
+            }
+            else {
+                squares[i][j].innerText = '';
+            }
+        }
+    }
+}
+
+function moveUp() {
+    for(let n = 0; n < 4; n++) {
+        for(let i = 1; i < values.length; i++) {
+            for(let j = 0; j < values[i].length; j++) {
+                if(values[i - 1][j] == values[i][j]) {
+                    values[i - 1][j] = values[i][j] * 2;
+                    values[i][j] = 0;
+                }
+                else if(values[i - 1][j] == 0) {
+                    values[i - 1][j] = values[i][j];
+                    values[i][j] = 0;
+                }
+            }
+        }
+    }
+    updateSquares();
+}
+
+function moveDown() {
+    for(let n = 0; n < 4; n++) {
+        for(let i = 2; i >= 0; i--) {
+            for(let j = 0; j < values[i].length; j++) {
+                if(values[i + 1][j] == values[i][j]) {
+                    values[i + 1][j] = values[i][j] * 2;
+                    values[i][j] = 0;
+                }
+                else if(values[i + 1][j] == 0) {
+                    values[i + 1][j] = values[i][j];
+                    values[i][j] = 0;
+                }
+            }
+        }
+    }
+    updateSquares();
+}
+
+function moveLeft() {
+    for(let n = 0; n < 4; n++) {
+        for(let j = 1; j < values[0].length; j++) {
+            for(let i = 0; i < values.length; i++) {
+                if(values[i][j - 1] == values[i][j]) {
+                    values[i][j - 1] = values[i][j] * 2;
+                    values[i][j] = 0;
+                }
+                else if(values[i][j - 1] == 0) {
+                    values[i][j - 1] = values[i][j];
+                    values[i][j] = 0;
+                }
+            }
+        }
+    }
+    updateSquares();
+}
+
+function moveRight() {
+    for(let n = 0; n < 4; n++) {
+        for(let j = 2; j >= 0; j--) {
+            for(let i = 0; i < values.length; i++) {
+                if(values[i][j + 1] == values[i][j]) {
+                    values[i][j + 1] = values[i][j] * 2;
+                    values[i][j] = 0;
+                }
+                else if(values[i][j + 1] == 0) {
+                    values[i][j + 1] = values[i][j];
+                    values[i][j] = 0;
+                }
+            }
+        }
+    }
+    updateSquares();
+}
+
+document.addEventListener('keydown', (event) => {
+    switch(event.key) {
+        case 'ArrowUp':
+            moveUp();
+            break;
+        case 'ArrowDown':
+            moveDown();
+            break;
+        case 'ArrowLeft':
+            moveLeft();
+            break;
+        case 'ArrowRight':
+            moveRight();
+            break;
+    }
+});
+
+function init() {
+    initGrid();
+}
+
+init();
+
+function test1() {
+    values[0][0] = 2;
+    values[2][2] = 2;
+    values[2][1] = 4;
+    values[1][0] = 4;
+    updateSquares();
+}
+
+test1();
